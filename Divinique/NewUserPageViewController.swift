f5f4ea764597be95188e970e252b8f93b0872733
lewiscai2003 <lcai0015@student.monash.edu>
2024-05-01 18:19:56 +1000

    new user page

--- Divinique/NewUserPageViewController.swift
+++ Divinique/NewUserPageViewController.swift
@@ -9,47 +9,18 @@ import UIKit
 
 class NewUserPageViewController: UIViewController {
 
+    
     @IBOutlet weak var nameTextField: UITextField!
     
-    @IBOutlet weak var birthdayTextField: UITextField!
+
+    @IBOutlet weak var datePicker: UIDatePicker!
     
-    @IBOutlet weak var birthTimeTextField: UITextField!
-    
-    @IBOutlet weak var datePickerTextField: UITextField!
-    
-    let datePicker = UIDatePicker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.hidesBackButton = true
-        
-        // Configure date picker mode
-        datePicker.datePickerMode = .dateAndTime
-        
-        // Set minimum and maximum dates if needed
-        // datePicker.minimumDate = Date()
-        
-        // Assign date picker as input view to the text field
-        datePickerTextField.inputView = datePicker
-        
-        // Add a toolbar with "Done" button to dismiss the picker
-        addToolBar()
+        self.navigationItem.hidesBackButton = false
     }
     
-    func addToolBar() {
-        let toolbar = UIToolbar()
-        toolbar.sizeToFit()
-        
-        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTapped))
-        toolbar.setItems([doneButton], animated: false)
-        
-        datePickerTextField.inputAccessoryView = toolbar
-    }
-
-    @objc func doneButtonTapped() {
-        datePickerTextField.resignFirstResponder()
-    }
-
     /*
     // MARK: - Navigation
 
