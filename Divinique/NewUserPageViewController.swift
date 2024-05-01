628aa5cb8cf086c882dba2bf792025b81f9debad
lewiscai2003 <lcai0015@student.monash.edu>
2024-05-01 18:37:03 +1000

    login and signup finished

--- Divinique/NewUserPageViewController.swift
+++ Divinique/NewUserPageViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+
 
 class NewUserPageViewController: UIViewController {
 
@@ -15,10 +17,48 @@ class NewUserPageViewController: UIViewController {
 
     @IBOutlet weak var datePicker: UIDatePicker!
     
-
+    
+    @IBAction func doneButton(_ sender: Any) {
+        
+        guard let name = nameTextField.text, !name.isEmpty else {
+            displayMessage(title: "Empty Name", message: "Please enter a name")
+            return
+        }
+        
+        let date = datePicker.date
+        
+        // Format the date as needed
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateString = dateFormatter.string(from: date)
+        
+        // Add user data to Firestore
+        let db = Firestore.firestore()
+        db.collection("users").addDocument(data: [
+            "name": name,
+            "date": dateString
+        ]) { error in
+            if let error = error {
+                self.displayMessage(title: "Error", message: "Something went wrong with database, try again later")
+            } else {
+                print("Document added successfully")
+            }
+        }
+        
+        performSegue(withIdentifier: "newUserToHome", sender: Self.self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.hidesBackButton = false
+        self.navigationItem.hidesBackButton = true
+    }
+    
+    func displayMessage(title: String, message: String){
+            let alertController = UIAlertController(title: title, message: message,
+             preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: .default,
+             handler: nil))
+            self.present(alertController, animated: true, completion: nil)
     }
     
     /*
